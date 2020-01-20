9cf0a6165c9a39109c8b2699a3cc0eea2ce3ef05
root <admin@vpntech.net>
2020-01-20 12:10:17 -0500

    automated sync

--- paramiko_sudo.py
+++ paramiko_sudo.py
@@ -5,14 +5,18 @@ from optparse import OptionParser
 
 PTY_ENV = {'abc':'def','wow':123}
 
-PTY_TERM = 'xterm-256'
+REMOTE_FORWARDED_PORT = 2233
+FORWARDED_PORT_DEST = 49552
+PTY_TERM = 'xterm'
 PTY_WIDTH = 160
 PTY_HEIGHT = 24
 PTY_WIDTH_PIXELS = 0
 PTY_HEIGHT_PIXELS = 0    
+EXEC_TIMEOUT = 10
+TUNNELS = {}
 
 EXEC_TIMEOUT = 10
-DSTAT = 'dstat -alp --top-cpu --top-cputime-avg 5 1500'
+DSTAT = 'dstat -alp --top-cpu --top-cputime-avg 1'
 SSH_PORT = 22
 HELP = """\
 Paramiko SSH Test
@@ -21,14 +25,120 @@ Paramiko SSH Test
 COMMANDS = ['id','ls /','find /']
 COMMANDS = ['id','ls /','tail -f /var/log/messages']
 COMMANDS = ['id','ls /','journalctl -f']
-COMMANDS = ['id','ls /a']
 COMMANDS = ['id','ls /','env']
+COMMANDS = ['id','ls /']
 COMMANDS = ['id','ls /',DSTAT]
 
 SH_ARGS = "i"
-COMMAND = ' && '.join(COMMANDS)
+#COMMAND = ' && '.join(COMMANDS)
 SUDO_ARGS='-k -H -u root'
 g_verbose = True
+
+def generateSudoCommand(COMMAND):
+    return "command sudo {} {} sh -{}c \"{}\"".format(SUDO_ARGS, generateEnvironmentString(), SH_ARGS, COMMAND)
+
+def exec_tunnel(ssh,cmd):
+    print('et.........')
+    stdin, stdout, stderr = ssh.exec_command(cmd);
+    for line in stdout:
+        print('[{}] => '.format(cmd) + line.strip('\n'))
+
+def handler(chan, host, port):
+    sock = socket.socket()
+    try:
+        sock.connect((host, port))
+    except Exception as e:
+        verbose("Forwarding request to %s:%d failed: %r" % (host, port, e))
+        return
+
+    verbose(
+        "Connected!  Tunnel open %r -> %r -> %r"
+        % (chan.origin_addr, chan.getpeername(), (host, port))
+    )
+    while True:
+        r, w, x = select.select([sock, chan], [], [])
+        if sock in r:
+            data = sock.recv(1024)
+            if len(data) == 0:
+                break
+            chan.send(data)
+        if chan in r:
+            data = chan.recv(1024)
+            if len(data) == 0:
+                break
+            sock.send(data)
+    chan.close()
+    sock.close()
+    verbose("Tunnel closed from %r" % (chan.origin_addr,))
+
+
+class reverse_forward_tunnel(threading.Thread):
+  def __init__(self,server_port, remote_host, remote_port, transport):
+    threading.Thread.__init__(self)
+    self.kill_received = False
+    self.server_port = server_port
+    self.remote_host = remote_host
+    self.remote_port = remote_port
+    self.transport = transport
+  def run(self):
+    self.transport.request_port_forward("", self.server_port)
+    while True:
+        chan = self.transport.accept(1000)
+        if chan is None:
+            continue
+        thr = threading.Thread(
+            target=handler, args=(chan, self.remote_host, self.remote_port)
+        )
+        thr.setDaemon(True)
+        thr.start()
+
+
+class __socat(threading.Thread):
+  def __init__(self, ssh, SOCAT_FILE):
+    threading.Thread.__init__(self)
+    self.kill_received = False
+    self.ssh = ssh
+    self.SOCAT_FILE = SOCAT_FILE
+  def run(self):
+    cmd = 'socat -u FILE:{},ignoreeof,seek-end tcp:127.0.0.1:2233'.format(self.SOCAT_FILE)
+    time.sleep(2.0)
+
+
+    session = self.ssh.get_transport().open_session()
+    session.set_combine_stderr(True)
+    session.get_pty(PTY_TERM,PTY_WIDTH, PTY_HEIGHT, PTY_WIDTH_PIXELS, PTY_HEIGHT_PIXELS)
+    session.settimeout(EXEC_TIMEOUT)
+ #   session.exec_command(generateSudoCommand(cmd))
+    exec_tunnel(self.ssh, generateSudoCommand(cmd))
+
+#    exec_tunnel(self.ssh, cmd)
+
+
+class __localSocat(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    self.kill_received = False
+  def run(self):
+    cmd = 'socat -u TCP4-LISTEN:49225,reuseaddr CREATE:/tmp/6c1bf75e-0733-4738-b552-a1118e12c61e/audit.json,perm=0640'
+    cwd = '/'
+    env = os.environ.copy()
+    time.sleep(2.0)
+    proc = subprocess.Popen(cmd.split(' '),stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env=env, shell=False)
+    stdout, stderr = proc.communicate()
+    exit_code = proc.wait()
+
+class __ls(threading.Thread):
+  def __init__(self, ssh):
+    threading.Thread.__init__(self)
+    self.kill_received = False
+    self.ssh = ssh
+  def run(self):
+    cmd = 'ls /'
+    exec_tunnel(self.ssh, cmd)
+
+
+
+
 
 def generateEnvironmentString():
     PTY_ENV_STR = ''
@@ -36,8 +146,6 @@ def generateEnvironmentString():
         PTY_ENV_STR += ' {}={}'.format(k,PTY_ENV[k])
     return PTY_ENV_STR
 
-def generateSudoCommand():
-    return "command sudo {} {} sh -{}c \"{}\"".format(SUDO_ARGS, generateEnvironmentString(), SH_ARGS, COMMAND)
 
 def verbose(s):
     if g_verbose:
@@ -136,12 +244,35 @@ def main():
         look_for_keys=True,
     )    
 
-    transport = ssh.get_transport()
-    session = transport.open_session()
+    ls = __ls(ssh)
+    ls.daemon = False
+    ls.start()
+
+    localSocat = __localSocat()
+    localSocat.daemon = True
+    localSocat.start()
+
+
+    SOCAT_FILE = '/var/log/messages'
+    agent = __socat(ssh, SOCAT_FILE)
+    agent.daemon = True
+    agent.start()
+
+
+    TUNNELS['json_audit'] = reverse_forward_tunnel(REMOTE_FORWARDED_PORT, remote[0], FORWARDED_PORT_DEST, ssh.get_transport())
+    TUNNELS['json_audit'].daemon = True
+    TUNNELS['json_audit'].start()
+    print(">> [json_audit Tunnel] Started")
+
+    #reverse_forward_tunnel(REMOTE_FORWARDED_PORT, remote[0], FORWARDED_PORT_DEST, ssh.get_transport())
+
+
+
+    session = ssh.get_transport().open_session()
     session.set_combine_stderr(True)
     session.get_pty(PTY_TERM,PTY_WIDTH, PTY_HEIGHT, PTY_WIDTH_PIXELS, PTY_HEIGHT_PIXELS)
-    session.settimeout(10)
-    session.exec_command(generateSudoCommand())
+    session.settimeout(EXEC_TIMEOUT)
+    session.exec_command(generateSudoCommand(DSTAT))
 
     stdin = session.makefile('wb', -1)
     stdout = session.makefile('rb', -1)
